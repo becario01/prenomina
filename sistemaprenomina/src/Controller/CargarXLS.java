9e040ee08493097a529710be649c4f3dc2743c46
programacion04 <programacion04@zunfeld.com>
2018-04-20 09:48:01 -0500

    Merge origin/master
    Conflicts:
    	sistemaprenomina/src/BD/login.java

--- sistemaprenomina/src/Controller/CargarXLS.java
+++ sistemaprenomina/src/Controller/CargarXLS.java
@@ -50,6 +50,7 @@ public class CargarXLS {
       public static Vector<String> arrayfecha = new Vector<>();
       public static Vector<String> arrayidemp = new Vector<String>();
       public static Vector<String> arrayid = new Vector<String>();
+      public static Vector<String> arraynombre = new Vector<String>();
 
       public void cargarempleados() throws IOException, SQLException, ParseException, BiffException {
 
@@ -59,11 +60,11 @@ public class CargarXLS {
 
             int rows = 0;
             FileNameExtensionFilter filter1 = new FileNameExtensionFilter("Doc - MS-Office 2003", "xls");
-            FileNameExtensionFilter filter2 = new FileNameExtensionFilter("Docx - MS-Office 2007", "xlsx");
+//            FileNameExtensionFilter filter2 = new FileNameExtensionFilter("Docx - MS-Office 2007", "xlsx");
             JFileChooser explorador = new JFileChooser();
             explorador.setDialogTitle("Abrir documento...");
             explorador.setFileFilter(filter1);
-            explorador.setFileFilter(filter2);
+            
 
             int seleccion = explorador.showDialog(null, "Abrir!");
 
@@ -87,81 +88,9 @@ public class CargarXLS {
 //y guardar una ruta
             String ruta = archivo.getPath();
 
-            if (explorador.getFileFilter() == filter2) {
-//                  Boolean com = false;
-//                  Boolean fin=false;
-//                  
-//                  String idfinal="";
-//                  String puestofinal="";
-//                  String deptofinal="";
-//                  int idempleado;
-//                  
-//                  try {
-//                        conn = (this.userConn != null) ? this.userConn : Conexion1.getConnection();
-//
-//                        FileInputStream file = new FileInputStream(new File(ruta));
-//
-//                        XSSFWorkbook wb = new XSSFWorkbook(file);
-//                        XSSFSheet sheet = wb.getSheetAt(0);
-//
-//                        int numFilas = sheet.getLastRowNum();
-//
-//                        for (int a = 1; a <= numFilas; a++) {
-//                              listaemp();
-//                              Row fila = sheet.getRow(a);
-//                              String sql = "INSERT INTO empleados( empleadoId, nombre, estatus, puesto, depto) VALUES (?,?,?,?,?)";
-//                              
-//                            String cadena=fila.getCell(1).getStringCellValue();
-//                            cadena="";
-//                              
-//                              if (fila.getCell(1).getStringCellValue()==null||"".equals(fila.getCell(1).getStringCellValue())||fila.getCell(1).getStringCellValue().length()==0) {
-//                                    idfinal="0";
-//                              }else{
-//                                    idfinal=fila.getCell(1).getStringCellValue();
-//                              }
-////                              if ("".equals(fila.getCell(2).getStringCellValue())) {
-////                                    puestofinal="";
-////                              } else {
-////                                    puestofinal=fila.getCell(2).getStringCellValue();
-////                              }
-////                              if ("".equals(fila.getCell(3).getStringCellValue())) {
-////                                    deptofinal="";
-////                              } else {
-////                                    deptofinal=fila.getCell(3).getStringCellValue();
-////                              }
-//                              stmt = conn.prepareStatement(sql);
-//                              stmt.setString(1, idfinal);
-//                              stmt.setString(2, fila.getCell(0).getStringCellValue());
-//                              stmt.setString(3, "1");
-//                              stmt.setString(4, fila.getCell(2).getStringCellValue());
-//                              stmt.setString(5, fila.getCell(3).getStringCellValue());
-//                              
-//                              
-//                               idempleado= Integer.parseInt(idfinal);
-//                               com = arrayidemp.contains(String.valueOf(idempleado));
-//                             if (idempleado == 0) {
-//                                          com = false;
-//                                    }
-//                               
-//
-//                                    if(com){
-//                                          System.out.println("ETE REGISTRO YA EXISTE EN LA BASE "); 
-//                                    }else{
-//
-//                                       stmt.execute();
-//                                    }
-//                        }
-//
-//                  } finally {
-//                        Conexion1.close(stmt);
-//                        if (this.userConn == null) {
-//                              Conexion1.close(conn);
-//                        }
-//
-//                  }
-
-            } else if (explorador.getFileFilter() == filter1) {
+             if (explorador.getFileFilter() == filter1) {
                   Boolean com = false;
+                  Boolean fin = false;
 
 
                   try {
@@ -182,11 +111,20 @@ public class CargarXLS {
                                     }
 
                                     int idempleado = Integer.parseInt(employedId);
+                                    String nomemp=nombre;
                                     listaemp();
 
                                     com = arrayidemp.contains(String.valueOf(idempleado));
                                     if (idempleado == 0) {
+                                        
                                           com = false;
+                                           for (int j = 0; j < arrayidemp.size(); j++) {
+                                          fin = arrayidemp.elementAt(j).equals(String.valueOf(idempleado)) && arraynombre.elementAt(j).equals(nomemp);
+                                          if (fin) {
+                                                com = true;
+                                          }
+                                    }
+                                          
                                     }
 
                                    
@@ -228,11 +166,11 @@ public class CargarXLS {
 
             int rows = 0;
             FileNameExtensionFilter filter1 = new FileNameExtensionFilter("Doc - MS-Office 2003", "xls");
-            FileNameExtensionFilter filter2 = new FileNameExtensionFilter("Docx - MS-Office 2007", "xlsx");
+//            FileNameExtensionFilter filter2 = new FileNameExtensionFilter("Docx - MS-Office 2007", "xlsx");
             JFileChooser explorador = new JFileChooser();
             explorador.setDialogTitle("Abrir documento...");
             explorador.setFileFilter(filter1);
-            explorador.setFileFilter(filter2);
+//            explorador.setFileFilter(filter2);
 
             int seleccion = explorador.showDialog(null, "Abrir!");
 
@@ -256,73 +194,7 @@ public class CargarXLS {
 //y guardar una ruta
             String ruta = archivo.getPath();
 
-            if (explorador.getFileFilter() == filter2) {
-//                  Boolean com = false;
-//                  Boolean fin=false;
-//                  listare();
-//                  String fechafinal;
-//                  System.out.println(filter2.getDescription() + "nf vnbskjnvkj");
-//                  try {
-//                        conn = (this.userConn != null) ? this.userConn : Conexion1.getConnection();
-//
-//                        FileInputStream file = new FileInputStream(new File(ruta));
-//
-//                        XSSFWorkbook wb = new XSSFWorkbook(file);
-//                        XSSFSheet sheet = wb.getSheetAt(0);
-//
-//                        int numFilas = sheet.getLastRowNum();
-//
-//                        for (int a = 1; a <= numFilas; a++) {
-//                              Row fila = sheet.getRow(a);
-//                              String sql = "INSERT INTO registros(empleadoId, Entrada, Salida, horas, fecha) VALUES (?,?,?,?,?)";
-//                              if (fila.getCell(4).getStringCellValue().length() == 0) {
-//                                    fechafinal = "1111-11-11";
-//                              } else {
-//
-//                                    SimpleDateFormat d = new SimpleDateFormat("dd/MM/yy");
-//                                    Date fe = d.parse(fila.getCell(4).getStringCellValue());
-//                                    System.out.println(fila.getCell(4).getStringCellValue() + "°°°");
-//                                    System.out.println("FECHA " + fe);
-//                                    SimpleDateFormat formatoDeFecha = new SimpleDateFormat("yyyy-MM-dd");
-//                                    fechafinal = formatoDeFecha.format(fe);
-//                                    System.out.println(formatoDeFecha.format(fe));
-//                              }
-//
-//                              stmt = conn.prepareStatement(sql);
-//                              stmt.setString(1, fila.getCell(0).getStringCellValue());
-//                              stmt.setString(2, fila.getCell(1).getStringCellValue());
-//                              stmt.setString(3, fila.getCell(2).getStringCellValue());
-//                              stmt.setString(4, fila.getCell(3).getStringCellValue());
-//                              stmt.setString(5, fechafinal);
-//          
-//                              
-//                               for (int j = 0; j < arrayid.size(); j++) {
-//                                          com = arrayid.elementAt(j).equals(fila.getCell(1).getStringCellValue()) && arrayfecha.elementAt(j).equals(fechafinal);
-//                                          if(com){
-//                                               fin=true; 
-//                                          }
-//                                          System.out.println(com);
-//                                          System.out.println(arrayid.elementAt(j));
-//                                          System.out.println(arrayfecha.elementAt(j));
-//                                    }
-//
-//                                    if(fin){
-//                                          System.out.println("ETE REGISTRO YA EXISTE EN LA BASE "); 
-//                                    }else{
-//
-//                                       stmt.execute();
-//                                    }
-//                        }
-//
-//                  } finally {
-//                        Conexion1.close(stmt);
-//                        if (this.userConn == null) {
-//                              Conexion1.close(conn);
-//                        }
-//
-//                  }
-
-            } else if (explorador.getFileFilter() == filter1) {
+         if (explorador.getFileFilter() == filter1) {
                   Boolean com = false;
 
                   listare();
@@ -445,6 +317,7 @@ public class CargarXLS {
             while (rs.next()) {
 
                   arrayidemp.add(con, rs.getString("empleadoId"));
+                  arraynombre.add(con, rs.getString("nombre"));
 
                   con++;
 //                                      
